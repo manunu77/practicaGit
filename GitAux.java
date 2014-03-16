7343395138f9ebdcb70c6a027a1228c6d8d8a007
AlvaroGRIS <alvarogutirodri@hotmail.com>
2014-03-16 17:39:06 +0100

    modificaciones alumno 2

--- GitAux.java
+++ GitAux.java
@@ -12,16 +12,16 @@ public class GitAux {
     
     public void metodoAlumno1(){
         System.out.println("metodo 1");
-	System.out.println("modificado por alumno 1"); // alumno 1
     }
     
     public void metodoAlumno2(){
         System.out.println("metodo 2");
+        System.out.println("modificado por alumno 2");
     }
     
     public void metodoComunitario(){
         System.out.println("Aqui escribimos todos");
-	// modificaciones alumno 1 
- 	System.out.println("Alumno 1 inserta esta sentencia");
+        //Modificaciones alumno 2"
+        System.out.println("Alumno 2 inserta esta sentencia");
     }
 }
