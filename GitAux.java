7f72fa82849449e47855d38bc8d2d8391df82834
Manuel Fco Álvarez Gaona <manunu77@hotmail.com>
2014-03-16 18:22:17 +0100

    subo otra vez Gitaux

--- GitAux.java
+++ GitAux.java
@@ -12,16 +12,17 @@ public class GitAux {
     
     public void metodoAlumno1(){
         System.out.println("metodo 1");
+        System.out.println("modificado por alumno 1");
     }
     
     public void metodoAlumno2(){
         System.out.println("metodo 2");
-        System.out.println("modificado por alumno 2");
+
     }
     
     public void metodoComunitario(){
         System.out.println("Aqui escribimos todos");
-        //Modificaciones alumno 2"
-        System.out.println("Alumno 2 inserta esta sentencia");
+        //Modificaciones alumno 1"
+        System.out.println("Alumno 1 inserta esta sentencia");
     }
 }
